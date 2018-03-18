7f3aed7bce2213af83eafc055a0142cf5af5da0d
h0mes <katiewood@Katies-MacBook-Pro.local>
2018-03-18 17:34:47 -0500

    p5.dom.js

--- home_no_5_west_backup.js
+++ home_no_5_west_backup.js
@@ -1,7 +1,7 @@
 function preload() {
   soundFormats('ogg');
-  forSouth = loadSound('/assets/home_no_5_west96k_forSouth.ogg');
-  forEast = loadSound('/assets/home_no_5_west96k_forEast.ogg');
+  forSouth = loadSound('h0mes.github.io/assets/home_no_5_west96k_forSouth.ogg');
+  forEast = loadSound('h0mes.github.io/assets/home_no_5_west96k_forEast.ogg');
 }
 
 var content_home5 = ["5.0 five", "5.1 Crows in the nearby-but-not-close, at least three", "5.2 There had been\n gabled second floor windows, green roof\n back porch covered, front porch open \n windows flanked the storm door \n a few steps up from the foundation.", "5.3 Here, desire rots seasons clean.", "5.4 Hear a cow lowing a few miles away. \n Pickup truck passes.", "5.5 Dead grass creases stiffly under my step. ", "5.6 In summer, conquered by paradise trees, it \n grew hot and pulpy, inside \n adopting the airy nature of its verdant exterior:", "5.7 rain and snow blowing diagonal into close-set second-story eyes, \n stiff wooden body softening with time and water, iterating decay.", "5.8 In its absence, I can see across the clearing \n to where the trees start,", "5.9 facing West. "];
