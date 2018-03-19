417eb953c6e508bd44263527c163a24edf2bbee4
h0mes <katiewood@Katies-MacBook-Pro.local>
2018-03-19 17:53:34 -0500

    rawgit.com

--- home_no_5_west.js
+++ home_no_5_west.js
@@ -7,8 +7,8 @@ function success() {
 
 function preload() {
   soundFormats('mp3', 'ogg');
-  forSouth = loadSound('http://etchasketch.fun/piano/piano.094.ogg', success);
-  //forEast = loadSound('https://raw.githubusercontent.com/h0mes/h0mes.github.io/master/assets/home_no_5_west96k_forEast.ogg');
+  forSouth = loadSound('https://rawgit.com/h0mes/h0mes.github.io/master/assets/home_no_5_west96k_forEast.ogg', success);
+  forEast = loadSound('https://rawgit.com/h0mes/h0mes.github.io/master/assets/home_no_5_west96k_forEast.ogg');
 }
 
 
@@ -32,9 +32,9 @@ oriented = function(e) {
 
 function setup() {
     forSouth.setVolume(0.6);
-    //forEast.setVolume(0.6);
+    forEast.setVolume(0.6);
     forSouth.playMode('untilDone');
-    //forEast.playMode('untilDone');
+    forEast.playMode('untilDone');
 var content_home5 = ["5.0 five", "5.1 Crows in the nearby-but-not-close, at least three", "5.2 There had been\n gabled second floor windows, green roof\n back porch covered, front porch open \n windows flanked the storm door \n a few steps up from the foundation.", "5.3 Here, desire rots seasons clean.", "5.4 Hear a cow lowing a few miles away. \n Pickup truck passes.", "5.5 Dead grass creases stiffly under my step. ", "5.6 In summer, conquered by paradise trees, it \n grew hot and pulpy, inside \n adopting the airy nature of its verdant exterior:", "5.7 rain and snow blowing diagonal into close-set second-story eyes, \n stiff wooden body softening with time and water, iterating decay.", "5.8 In its absence, I can see across the clearing \n to where the trees start,", "5.9 facing West. "];
 var content = content_home5;
 var text_area = document.getElementById("compass_content");
@@ -51,7 +51,7 @@ function myOrientation(){
 
 //East forEast.ogg
     if(compassHeading >= 45 && compassHeading < 135) {
-        //forEast.play();
+        forEast.play();
     }   
  //West flicker       
     if(compassHeading >= 225 && compassHeading < 315) {
