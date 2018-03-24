9fb2d21928ae513f25ea0359309481cb913ff7b0
h0mes <katiewood@Katies-MacBook-Pro.local>
2018-03-24 16:23:54 -0500

    volume

--- home_no_5_west.js
+++ home_no_5_west.js
@@ -7,8 +7,8 @@ function success() {
 
 function preload() {
  soundFormats('mp3', 'ogg');
-  forSouth = loadSound('assets/home_no_5_west_1minute.mp3', success);
- forEast = loadSound('assets/home_no_5_west_2minute.mp3');
+  forSouth = loadSound('assets/home_no_5_west_1min.mp3', success);
+ forEast = loadSound('assets/home_no_5_west_2min.mp3');
 };
 
 var text_area;
