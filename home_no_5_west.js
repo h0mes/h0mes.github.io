1b1480ab007eba59b8410afa9e64d04b262f8368
h0mes <katiewood@Katies-MacBook-Pro.local>
2018-03-23 20:33:13 -0500

    trying native JS touchstart and touchend events

--- home_no_5_west.js
+++ home_no_5_west.js
@@ -26,10 +26,14 @@ content_home5 = ["5.0 five", "5.1 Crows in the nearby-but-not-close, at least th
 auxText = ["01.mov: This is how home number five was the first time I saw it. It’s a bungalow, a low-rise house with a veranda and a small loft. The word bungalow comes from the Hindi word for “Bengali,”  the people of Bangladesh, where this architectural style originated in the 15th century. The person who owned this house probably wasn’t aware of its etymology. ", "02.mov Here’s the home the summer after that last image, three years before now. Those are paradise trees growing up the side of it. They are an invasive species in the American Southeast. At this point the home had been empty at least 10 years. ", "03.mov Here it is the following winter. My dad texted me this when I asked him if he knew anything about it: “Got signatures on petition to run for supervisor. 1996. Knocked on lots of doors, including that one. They were carving a deer carcass on the dining room table in the center room. Had killed it and field dressed it, were cutting the meat off that night. Sticks in my mind… Hand saw and two or three butcher knives. Most people do that work outside. That’s why I remember it. “", "04.mov Somebody must have inherited the property after the man living there died, because the home had been bulldozed by summer of 2017. \n What you don’t see in this sporadic record is the massive rural-to-urban migration that took place after the second world war, where a million civilians moved from farms to cities as a result of industrial reconversion. No doubt the children of this farmer where part of that migration, leaving behind a dwelling too simple to adapt and too cheap in construction to be worth resuscitating. \nYou can’t hear it in the field recording, but there’s a river right on the other side of this hill. You can’t see it from here either. I found out about it from looking for this place on google maps, thinking that the software would have enough of a lag in update for me to find another final image of the home. The last satellite capture for this area was from 2018, when the home was already gone. "];
 text_area = document.getElementById("compass_content");
 i = 0
-var clicky = function mousePressed(){
-             i++;
-             text_area.innerHTML = auxText[i];
-        };
+// var clicky = function mousePressed(){
+//              i++;
+//              text_area.innerHTML = auxText[i];
+//              return false;
+//              if(i >= 10) {
+//                i = 0; 
+//              } 
+//         };
 
 //var promise = FULLTILT.getDeviceOrientation({ 'type': 'world' });
     //promise.then(function(deviceOrientation) {
@@ -122,8 +126,16 @@ function draw(){
         if(forSouth.isPlaying() == true){
             forSouth.stop()
         }
-        clicky();
-        text_area.innerHTML = auxText[i]
-    }
+        // text_area.clicky();
+        text_area.addEventListener('touchstart', function(e){
+           i++;
+           text_area.innerHTML = auxText[i]; 
+            }
+        }, false)
+        text_area.addEventListener('touchend', function(e){
+           i++;
+           text_area.innerHTML = " "; 
+            }
+        }, false)
 }
 
